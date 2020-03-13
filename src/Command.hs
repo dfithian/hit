5b6c91fcd66cbe9dea84ce8432e78efe9928011e
Dan Fithian <dan.fithian@simspace.com>
2020-03-13 09:16:24 -0400

    Simplify the recursion so that we don't repeat ourselves

--- src/Command.hs
+++ src/Command.hs
@@ -8,34 +8,35 @@ import Turtle (FoldShell(FoldShell), (</>), FilePath, basename, foldShell, isDir
 
 import qualified Types as T
 
+data Recurse
+  = RecurseNone
+  | RecurseOnce
+  deriving (Eq, Ord, Show, Enum, Bounded)
+
+-- |Fold over directories and accumulate the ones that contain a `.git` folder or whose children contain a `.git` folder
+-- up to the specified recursion depth.
+foldGitDirs :: (Eq a, Bounded a, Enum a, MonadIO m) => a -> [FilePath] -> FilePath -> m [FilePath]
+foldGitDirs recurse accum next = do
+  nextStatus <- stat next
+  case (isDirectory nextStatus, basename next == ".git") of
+    (False, _) -> pure accum
+    (_, True) -> pure $ (parent next):accum
+    (True, False) -> case recurse == minBound of
+      True -> pure accum
+      False -> foldShell (ls next) (FoldShell (foldGitDirs (pred recurse)) accum pure)
+
 -- |Interpret subdirs. If a project is specified by `name` in `main.hs`, then we interpret the subdirs to be the ones
 -- from that project's config. If not, check the current working directory and return every child that has a `.git`
 -- subdirectory. If we're currently in a git repository, also return that one.
 interpretSubdirs :: MonadIO m => Maybe T.ConfigProject -> m [FilePath]
-interpretSubdirs projectMay = do
-  let isGitSubdir accum next = do
-        nextStatus <- stat next
-        case accum of
-          Nothing -> case isDirectory nextStatus && basename next == ".git" of
-            True -> pure $ Just next
-            False -> pure Nothing
-          isGit -> pure isGit
-      gitDir accum next = do
-        nextStatus <- stat next
-        case (isDirectory nextStatus, basename next == ".git") of
-          (False, False) -> pure accum
-          (_, True) -> pure $ (parent next):accum
-          (True, _) -> foldShell (ls next) (FoldShell isGitSubdir Nothing pure) >>= \ case
-            Just _ -> pure $ next:accum
-            Nothing -> pure accum
-  case projectMay of
-    Just project -> do
-      let projectHome = view T.configProjectHome project
-          projectDirs = view T.configProjectDirs project
-      pure $ (projectHome </>) <$> projectDirs
-    Nothing -> do
-      cwd <- pwd
-      foldShell (ls cwd) $ FoldShell gitDir [] pure
+interpretSubdirs = \ case
+  Just project -> do
+    let projectHome = view T.configProjectHome project
+        projectDirs = view T.configProjectDirs project
+    pure $ (projectHome </>) <$> projectDirs
+  Nothing -> do
+    cwd <- pwd
+    foldShell (ls cwd) (FoldShell (foldGitDirs RecurseOnce) [] pure)
 
 -- |Print a command from this program, always in blue and always unset after.
 putStrLnComment :: MonadIO m => Text -> m ()
