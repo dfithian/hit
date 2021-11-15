d7d1040ac70845e83ee4a66a5171c1c11f53b16e
Dan Fithian <dan.fithian@simspace.com>
2021-11-15 10:34:32 -0500

    Modernize the code, strip dependencies, use xdg directory

--- src/Command.hs
+++ src/Command.hs
@@ -1,10 +1,11 @@
 module Command where
 
-import ClassyPrelude hiding ((</>), FilePath, fold)
-import Control.Lens (view)
-import System.Console.ANSI (Color(Blue), ColorIntensity(Vivid), ConsoleLayer(Foreground), SGR(Reset, SetColor), setSGR)
-import System.IO (stdout)
-import Turtle (FoldShell(FoldShell), (</>), FilePath, basename, foldShell, isDirectory, ls, parent, pwd, stat)
+import Prelude
+
+import Data.Text (Text, unpack)
+import System.Directory (doesDirectoryExist, getCurrentDirectory, listDirectory)
+import System.FilePath.Posix ((</>), takeDirectory)
+import qualified Data.Text.ANSI as Text.ANSI
 
 import qualified Types as T
 
@@ -15,33 +16,32 @@ data Recurse
 
 -- |Fold over directories and accumulate the ones that contain a `.git` folder or whose children contain a `.git` folder
 -- up to the specified recursion depth.
-foldGitDirs :: (Eq a, Bounded a, Enum a, MonadIO m) => a -> [FilePath] -> FilePath -> m [FilePath]
+foldGitDirs :: (Eq a, Bounded a, Enum a) => a -> [FilePath] -> FilePath -> IO [FilePath]
 foldGitDirs recurse accum next = do
-  nextStatus <- stat next
-  case (isDirectory nextStatus, basename next == ".git") of
+  isDirectory <- doesDirectoryExist next
+  case (isDirectory, next == ".git") of
     (False, _) -> pure accum
-    (_, True) -> pure $ (parent next):accum
+    (_, True) -> pure $ (takeDirectory next):accum
     (True, False) -> case recurse == minBound of
       True -> pure accum
-      False -> foldShell (ls next) (FoldShell (foldGitDirs (pred recurse)) accum pure)
+      False -> do
+        dirs <- listDirectory next
+        mconcat <$> traverse (foldGitDirs (pred recurse) accum) dirs
 
 -- |Interpret subdirs. If a project is specified by `name` in `main.hs`, then we interpret the subdirs to be the ones
 -- from that project's config. If not, check the current working directory and return every child that has a `.git`
 -- subdirectory. If we're currently in a git repository, also return that one.
-interpretSubdirs :: MonadIO m => Maybe T.ConfigProject -> m [FilePath]
-interpretSubdirs = \ case
+interpretSubdirs :: Maybe T.ConfigProject -> IO [FilePath]
+interpretSubdirs = \case
   Just project -> do
-    let projectHome = view T.configProjectHome project
-        projectDirs = view T.configProjectDirs project
+    let projectHome = T.configProjectHome project
+        projectDirs = T.configProjectDirs project
     pure $ (projectHome </>) <$> projectDirs
   Nothing -> do
-    cwd <- pwd
-    foldShell (ls cwd) (FoldShell (foldGitDirs RecurseOnce) [] pure)
+    cwd <- getCurrentDirectory
+    dirs <- listDirectory cwd
+    mconcat <$> traverse (foldGitDirs RecurseOnce []) dirs
 
 -- |Print a command from this program, always in blue and always unset after.
-putStrLnComment :: MonadIO m => Text -> m ()
-putStrLnComment x = liftIO $ do
-  setSGR [SetColor Foreground Vivid Blue]
-  putStrLn $ "# " <> x
-  setSGR [Reset]
-  hFlush stdout -- flush so that the reset hits the terminal
+putStrLnComment :: Text -> IO ()
+putStrLnComment x = putStrLn $ unpack $ Text.ANSI.cyan $ "# " <> x
--- src/Types.hs
+++ src/Types.hs
@@ -1,36 +1,33 @@
 module Types where
 
-import ClassyPrelude hiding (FilePath)
-import Control.Lens.TH (makeLenses)
-import Data.Aeson ((.:), FromJSON, parseJSON, withArray, withObject)
-import Turtle (FilePath, decodeString)
+import Prelude
+
+import Data.Aeson ((.:), FromJSON, parseJSON, withObject)
+import Data.Map (Map)
+import Data.Text (Text)
+import qualified Data.HashMap.Strict as HashMap
+import qualified Data.Map.Strict as Map
 
 data Config = Config
-  { _configProjects :: [ConfigProject]
-  -- ^ The list of projects
+  { configProjects :: Map Text ConfigProject
+  -- ^ The list of projects.
   } deriving (Eq, Show)
 
 data ConfigProject = ConfigProject
-  { _configProjectName :: Text
-  -- ^ The name of this project, which we invoke using `hit <name>`.
-  , _configProjectHome :: FilePath
+  { configProjectHome :: FilePath
   -- ^ The home dir for this project, absolute path required.
-  , _configProjectDirs :: [FilePath]
+  , configProjectDirs :: [FilePath]
   -- ^ The subdirs for this project.
   } deriving (Eq, Show)
 
-makeLenses ''Config
-makeLenses ''ConfigProject
-
 defaultConfig :: Config
-defaultConfig = Config []
+defaultConfig = Config mempty
 
 instance FromJSON ConfigProject where
-  parseJSON = withObject "ConfigProject" $ \ obj -> ConfigProject
-    <$> obj .: "name"
-    <*> (decodeString <$> obj .: "home")
-    <*> map (map decodeString) (obj .: "dirs")
+  parseJSON = withObject "ConfigProject" $ \obj -> ConfigProject
+    <$> obj .: "home"
+    <*> obj .: "dirs"
 
 instance FromJSON Config where
-  parseJSON = withArray "Config" $ \ arr ->
-    Config . toList <$> traverse parseJSON arr
+  parseJSON = withObject "Config" $ \obj ->
+    Config . Map.fromList . HashMap.toList <$> traverse parseJSON obj
